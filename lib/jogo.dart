b556c65d269c4a574d1a2067dceada2de4c7eddd
Jeffc-98 <jefferson_am1998@hotmail.com>
2026-04-27 01:38:11 -0300

    App finalizado e funcional

--- lib/jogo.dart
+++ lib/jogo.dart
@@ -1,8 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:jakenpo_v2/Jogo.dart';
+import 'dart:math';
 
-class Jogo extends StatelessWidget {
-  const Jogo({super.key, required MaterialColor backgroundColor});
+class Jogo extends StatefulWidget {
+  const Jogo({super.key});
+
+  @override
+  State<Jogo> createState() => _JogoState();
+}
+
+class _JogoState extends State<Jogo> {
+  var _imagemApp = AssetImage("imagens/padrao.png");
+  var _mensagem = "Escolha uma opção abaixo!";
+
+  void _opcqoSelecionada(String escolhaUsuario) {
+    print("Opção selecionada: $escolhaUsuario");
+
+    var opcao = ["Pedra", "Papel", "Tesoura"];
+    var numero = Random().nextInt(3);
+    var escolhaApp = opcao[numero];
+
+    switch (escolhaApp) {
+      case "Pedra":
+        setState(() {
+          _imagemApp = AssetImage("imagens/pedra.png");
+        });
+        break;
+      case "Papel":
+        setState(() {
+          _imagemApp = AssetImage("imagens/papel.png");
+        });
+        break;
+
+      case "Tesoura":
+        setState(() {
+          _imagemApp = AssetImage("imagens/tesoura.png");
+        });
+        break;
+    }
+    if (escolhaUsuario == "Pedra" && escolhaApp == "Tesoura" ||
+        escolhaUsuario == "Tesoura" && escolhaApp == "Papel" ||
+        escolhaUsuario == "Papel" && escolhaApp == "Pedra") {
+      setState(() {
+        _mensagem = "Você ganhou";
+      });
+    } else if (escolhaApp == "Pedra" && escolhaUsuario == "Tesoura" ||
+        escolhaApp == "Tesoura" && escolhaUsuario == "Papel" ||
+        escolhaApp == "Papel" && escolhaUsuario == "Pedra") {
+      setState(() {
+        _mensagem = "Você perdeu!";
+      });
+    } else {
+      setState(() {
+        _mensagem = "Empatamos!";
+      });
+    }
+
+    print("Escolha do App: $escolhaApp");
+    print("Escolha do Usuário: $escolhaUsuario");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +74,11 @@ class Jogo extends StatelessWidget {
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
           ),
-          Image.asset("imagens/padrao.png"),
+          Image(image: _imagemApp),
           Padding(
             padding: EdgeInsets.only(top: 32, bottom: 16),
-            child: const Text(
-              'Escolha uma opção abaixo:',
+            child: Text(
+              _mensagem,
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
             ),
@@ -31,12 +86,20 @@ class Jogo extends StatelessWidget {
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
-              Image.asset("imagens/pedra.png", height: 95,),
-              Image.asset("imagens/papel.png", height: 95,),
-              Image.asset("imagens/tesoura.png", height: 95,)
+              GestureDetector(
+                onTap: () => _opcqoSelecionada("Pedra"),
+                child: Image.asset("imagens/pedra.png", height: 95),
+              ),
+              GestureDetector(
+                onTap: () => _opcqoSelecionada("Papel"),
+                child: Image.asset("imagens/papel.png", height: 95),
+              ),
+              GestureDetector(
+                onTap: () => _opcqoSelecionada("Tesoura"),
+                child: Image.asset("imagens/tesoura.png", height: 95),
+              ),
             ],
-          )
-
+          ),
         ],
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:jakenpo_v2/jogo.dart';
 
 void main(){
   runApp(MaterialApp(
-    home: Jogo(backgroundColor:   Colors.blue)
+    home: Jogo(),
   ));
 
 }
